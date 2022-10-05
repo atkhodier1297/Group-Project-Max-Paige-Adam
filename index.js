459bfbece7f4ea88f454e4c193def1e9f1779b4c
Adam Khodier <khodieradam1297@gmail.com>
2022-10-05 10:38:32 -0400

    spotify top 20

--- index.js
+++ index.js
@@ -2,11 +2,11 @@ const options = {
 	method: 'GET',
 	headers: {
 		'X-RapidAPI-Key': 'd6bf0d4287msh34e784f611f1ab0p1cdaa4jsnadcb166d5ad1',
-		'X-RapidAPI-Host': 'genius-song-lyrics1.p.rapidapi.com'
+		'X-RapidAPI-Host': 'spotify81.p.rapidapi.com'
 	}
 };
 
-fetch('https://genius-song-lyrics1.p.rapidapi.com/albums/368574', options)
+fetch('https://spotify81.p.rapidapi.com/top_20_by_monthly_listeners', options)
 	.then(response => response.json())
 	.then(response => console.log(response))
 	.catch(err => console.error(err));
